b7e009dfb4b3cfc911e793cb06fdf7c3e1f2844f
huuthanh2k4 <143076709+huuthanh2k4@users.noreply.github.com>
2024-12-30 23:01:06 +0700

    Update Thành (Unshared)__1.sql

--- Project_2/src/main/webapp/resources/SQL/Thành (Unshared)__1.sql
+++ Project_2/src/main/webapp/resources/SQL/Thành (Unshared)__1.sql
@@ -209,39 +209,39 @@ CREATE TABLE danhsachsp (
     dungLgPin NVARCHAR2(50) DEFAULT 'Chưa có thông tin !!!',
     HeDieuHanh NVARCHAR2(50) DEFAULT 'Android',
     maThuongHieu NVARCHAR2(10) NOT NULL,FOREIGN KEY (maThuongHieu) REFERENCES thuongHieu(maThuongHieu),
-    maDanhMuc NVARCHAR2(10) NOT NULL,FOREIGN KEY (maDanhMuc) REFERENCES DanhMuc(maDanhMuc)
+    maDanhMuc NVARCHAR2(10) NOT NULL,FOREIGN KEY (maDanhMuc) REFERENCES DanhMuc(maDanhMuc),
+    iMGNOIBAT NVARCHAR2(100)
 );
 
 
-INSERT INTO danhsachsp(idSP, tenSP, moTaSP, img, dungLgPin,HeDieuHanh,maThuongHieu, maDanhMuc) 
-VALUES (N'SP001', N'iPhone 15 Pro Max', N'Điện thoại thông minh cao cấp nhất của Apple với chip A17 Bionic mạnh mẽ, màn hình Super Retina XDR sống động và hệ thống camera tiên tiến.', N'iphone-15-pro-max-1.jpg', N'4352mAh',N'IOS', N'TH001', N'DM01');
+INSERT INTO danhsachsp(idSP, tenSP, moTaSP, img, dungLgPin,HeDieuHanh,maThuongHieu, maDanhMuc,IMGNOIBAT) 
+VALUES (N'SP001', N'iPhone 15 Pro Max', N'Điện thoại thông minh cao cấp nhất của Apple với chip A17 Bionic mạnh mẽ, màn hình Super Retina XDR sống động và hệ thống camera tiên tiến.', N'iphone-15-pro-max-1.jpg', N'4352mAh',N'IOS', N'TH001', N'DM01','iPhone 15 Pro Max NB');
 
-INSERT INTO danhsachsp(idSP, tenSP, moTaSP, img, dungLgPin,HeDieuHanh,maThuongHieu, maDanhMuc) 
-VALUES (N'SP002', N'Samsung Galaxy S23 Ultra', N'Điện thoại flagship của Samsung với màn hình Dynamic AMOLED 2X tuyệt đẹp, camera 200MP đỉnh cao và hiệu năng vượt trội nhờ chip Snapdragon 8 Gen 2.', N'samsung-galaxy-s23-1.jpg', N'5000mAh',N'Android', N'TH002', N'DM01');
+INSERT INTO danhsachsp(idSP, tenSP, moTaSP, img, dungLgPin,HeDieuHanh,maThuongHieu, maDanhMuc,IMGNOIBAT) 
+VALUES (N'SP002', N'Samsung Galaxy S23 Ultra', N'Điện thoại flagship của Samsung với màn hình Dynamic AMOLED 2X tuyệt đẹp, camera 200MP đỉnh cao và hiệu năng vượt trội nhờ chip Snapdragon 8 Gen 2.', N'samsung-galaxy-s23-1.jpg', N'5000mAh',N'Android', N'TH002', N'DM01','Samsung Galaxy S23 Ultra NB');
 
-INSERT INTO danhsachsp(idSP, tenSP, moTaSP, img, dungLgPin,HeDieuHanh,maThuongHieu, maDanhMuc) 
-VALUES (N'SP003', N'Xiaomi Redmi Note 12 Pro', N'Điện thoại tầm trung của Xiaomi với màn hình AMOLED 120Hz mượt mà, camera 50MP chất lượng và hiệu năng ổn định nhờ chip MediaTek Dimensity 1080.', N'xiaomi-redmi-note-12-pro-1.jpg', N'5000mAh',N'Android', N'TH003', N'DM01');
+INSERT INTO danhsachsp(idSP, tenSP, moTaSP, img, dungLgPin,HeDieuHanh,maThuongHieu, maDanhMuc,IMGNOIBAT) 
+VALUES (N'SP003', N'Xiaomi Redmi Note 12 Pro', N'Điện thoại tầm trung của Xiaomi với màn hình AMOLED 120Hz mượt mà, camera 50MP chất lượng và hiệu năng ổn định nhờ chip MediaTek Dimensity 1080.', N'xiaomi-redmi-note-12-pro-1.jpg', N'5000mAh',N'Android', N'TH003', N'DM01','xiaomi-redmi-note-12-pro-5g638162920733414283');
 
-INSERT INTO danhsachsp(idSP, tenSP, moTaSP, img, dungLgPin,HeDieuHanh,maThuongHieu, maDanhMuc) 
-VALUES (N'SP004', N'Vivo V27 Pro', N'Điện thoại Vivo tập trung vào camera với thiết kế đẹp mắt, màn hình AMOLED 120Hz và camera chính 50MP với chống rung quang học OIS.', N'vivo-v27-pro-1.jpg', N'4600mAh',N'Android', N'TH004', N'DM01');
+INSERT INTO danhsachsp(idSP, tenSP, moTaSP, img, dungLgPin,HeDieuHanh,maThuongHieu, maDanhMuc,IMGNOIBAT) 
+VALUES (N'SP004', N'Vivo V27 Pro', N'Điện thoại Vivo tập trung vào camera với thiết kế đẹp mắt, màn hình AMOLED 120Hz và camera chính 50MP với chống rung quang học OIS.', N'vivo-v27-pro-1.jpg', N'4600mAh',N'Android', N'TH004', N'DM01','vivo NB');
 
-INSERT INTO danhsachsp(idSP, tenSP, moTaSP, img, dungLgPin,HeDieuHanh,maThuongHieu, maDanhMuc) 
-VALUES (N'SP005', N'Oppo Reno10 Pro+', N'Điện thoại Oppo với thiết kế mỏng nhẹ, màn hình AMOLED 120Hz, camera chân dung tele 32MP và sạc nhanh SuperVOOC 100W.', N'oppo-reno10-pro-1.jpg', N'4700mAh',N'Android', N'TH005', N'DM01');
+INSERT INTO danhsachsp(idSP, tenSP, moTaSP, img, dungLgPin,HeDieuHanh,maThuongHieu, maDanhMuc,IMGNOIBAT) 
+VALUES (N'SP005', N'Oppo Reno10 Pro+', N'Điện thoại Oppo với thiết kế mỏng nhẹ, màn hình AMOLED 120Hz, camera chân dung tele 32MP và sạc nhanh SuperVOOC 100W.', N'oppo-reno10-pro-1.jpg', N'4700mAh',N'Android', N'TH005', N'DM01','vi-vn-oppo-reno10-pro-5g-slider-(1)');
 
-INSERT INTO danhsachsp(idSP, tenSP, moTaSP, img, dungLgPin,HeDieuHanh,maThuongHieu, maDanhMuc) 
-VALUES (N'SP006', N'iPhone 14', N'Điện thoại iPhone thế hệ trước với chip A15 Bionic vẫn mạnh mẽ, màn hình Super Retina XDR và hệ thống camera kép chất lượng.', N'iPhone-14-1.jpg', N'3279mAh',N'IOS', N'TH001', N'DM01');
+INSERT INTO danhsachsp(idSP, tenSP, moTaSP, img, dungLgPin,HeDieuHanh,maThuongHieu, maDanhMuc,IMGNOIBAT) 
+VALUES (N'SP006', N'iPhone 14', N'Điện thoại iPhone thế hệ trước với chip A15 Bionic vẫn mạnh mẽ, màn hình Super Retina XDR và hệ thống camera kép chất lượng.', N'iPhone-14-1.jpg', N'3279mAh',N'IOS', N'TH001', N'DM01','iphone-14-256gb638176904598985648');
+INSERT INTO danhsachsp(idSP, tenSP, moTaSP, img, dungLgPin,HeDieuHanh,maThuongHieu, maDanhMuc,IMGNOIBAT) 
+VALUES (N'SP007', N'Samsung Galaxy A54', N'Điện thoại tầm trung của Samsung với màn hình Super AMOLED 120Hz, camera chính 50MP với chống rung quang học OIS và pin lớn 5000mAh.', N'samsung-galaxy-a54-t1.jpg', N'5000mAh',N'Android', N'TH002', N'DM02','vi-vn-samsung-galaxy-a54-5g-slider--(2)');
 
-INSERT INTO danhsachsp(idSP, tenSP, moTaSP, img, dungLgPin,HeDieuHanh,maThuongHieu, maDanhMuc) 
-VALUES (N'SP007', N'Samsung Galaxy A54', N'Điện thoại tầm trung của Samsung với màn hình Super AMOLED 120Hz, camera chính 50MP với chống rung quang học OIS và pin lớn 5000mAh.', N'samsung-galaxy-a54-t1.jpg', N'5000mAh',N'Android', N'TH002', N'DM02');
+INSERT INTO danhsachsp(idSP, tenSP, moTaSP, img, dungLgPin,HeDieuHanh,maThuongHieu, maDanhMuc,IMGNOIBAT) 
+VALUES (N'SP008', N'Oppo A78', N'Điện thoại Oppo giá rẻ với màn hình AMOLED 90Hz, camera chính 50MP và sạc nhanh 33W.', N'oppo-a78-1.jpg', N'5000mAh',N'Android', N'TH005', N'DM01','oppo-a78-slider-1a-1020x570');
 
-INSERT INTO danhsachsp(idSP, tenSP, moTaSP, img, dungLgPin,HeDieuHanh,maThuongHieu, maDanhMuc) 
-VALUES (N'SP008', N'Oppo A78', N'Điện thoại Oppo giá rẻ với màn hình AMOLED 90Hz, camera chính 50MP và sạc nhanh 33W.', N'oppo-a78-1.jpg', N'5000mAh',N'Android', N'TH005', N'DM01');
+INSERT INTO danhsachsp(idSP, tenSP, moTaSP, img, dungLgPin,HeDieuHanh,maThuongHieu, maDanhMuc,IMGNOIBAT) 
+VALUES (N'SP009', N'Xiaomi 13', N'Điện thoại flagship của Xiaomi với thiết kế cao cấp, màn hình AMOLED 120Hz, camera Leica và hiệu năng mạnh mẽ nhờ chip Snapdragon 8 Gen 2.', N'xiaomi-redmi-note-13-pro-1.jpg', N'4500mAh',N'Android', N'TH003', N'DM01','vi-vn-xiaomi-redmi-13-1');
 
-INSERT INTO danhsachsp(idSP, tenSP, moTaSP, img, dungLgPin,HeDieuHanh,maThuongHieu, maDanhMuc) 
-VALUES (N'SP009', N'Xiaomi 13', N'Điện thoại flagship của Xiaomi với thiết kế cao cấp, màn hình AMOLED 120Hz, camera Leica và hiệu năng mạnh mẽ nhờ chip Snapdragon 8 Gen 2.', N'xiaomi-redmi-note-13-pro-1.jpg', N'4500mAh',N'Android', N'TH003', N'DM01');
-
-INSERT INTO danhsachsp(idSP, tenSP, moTaSP, img, dungLgPin,HeDieuHanh,maThuongHieu, maDanhMuc) 
-VALUES (N'SP010', N'Vivo Y36', N'Điện thoại Vivo tầm trung với màn hình LCD 90Hz, camera chính 50MP và pin lớn 5000mAh.', N'vivo-y36-1.jpg', N'5000mAh',N'Android', N'TH004', N'DM01');
+INSERT INTO danhsachsp(idSP, tenSP, moTaSP, img, dungLgPin,HeDieuHanh,maThuongHieu, maDanhMuc,IMGNOIBAT) 
+VALUES (N'SP010', N'Vivo Y36', N'Điện thoại Vivo tầm trung với màn hình LCD 90Hz, camera chính 50MP và pin lớn 5000mAh.', N'vivo-y36-1.jpg', N'5000mAh',N'Android', N'TH004', N'DM01','vivo_y36_-_1_1_1_1');
 
 
 
@@ -392,6 +392,70 @@ INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (60, TO_DATE('2023-09-15', 'YYY
 INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (60, TO_DATE('2024-06-10', 'YYYY-MM-DD'), 17500000);
 INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (61, TO_DATE('2022-02-15', 'YYYY-MM-DD'), 15000000);
 INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (62, TO_DATE('2022-12-05', 'YYYY-MM-DD'), 20000000);
+
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (1, TO_DATE('2022-03-15', 'YYYY-MM-DD'), 15000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (2, TO_DATE('2023-07-19', 'YYYY-MM-DD'), 25000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (3, TO_DATE('2024-01-10', 'YYYY-MM-DD'), 18000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (4, TO_DATE('2022-09-05', 'YYYY-MM-DD'), 30000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (5, TO_DATE('2023-11-25', 'YYYY-MM-DD'), 22000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (6, TO_DATE('2024-04-18', 'YYYY-MM-DD'), 26000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (7, TO_DATE('2022-06-09', 'YYYY-MM-DD'), 12000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (8, TO_DATE('2023-03-30', 'YYYY-MM-DD'), 17000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (9, TO_DATE('2024-08-14', 'YYYY-MM-DD'), 20000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (10, TO_DATE('2022-12-20', 'YYYY-MM-DD'), 14000000);
+
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (1, TO_DATE('2022-03-15', 'YYYY-MM-DD'), 15000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (2, TO_DATE('2023-07-19', 'YYYY-MM-DD'), 25000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (3, TO_DATE('2024-01-10', 'YYYY-MM-DD'), 18000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (4, TO_DATE('2022-09-05', 'YYYY-MM-DD'), 30000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (5, TO_DATE('2023-11-25', 'YYYY-MM-DD'), 22000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (6, TO_DATE('2024-04-18', 'YYYY-MM-DD'), 26000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (7, TO_DATE('2022-06-09', 'YYYY-MM-DD'), 12000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (8, TO_DATE('2023-03-30', 'YYYY-MM-DD'), 17000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (9, TO_DATE('2024-08-14', 'YYYY-MM-DD'), 20000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (10, TO_DATE('2022-12-20', 'YYYY-MM-DD'), 14000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (11, TO_DATE('2023-02-10', 'YYYY-MM-DD'), 16000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (12, TO_DATE('2024-06-21', 'YYYY-MM-DD'), 23000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (13, TO_DATE('2022-05-14', 'YYYY-MM-DD'), 20000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (14, TO_DATE('2023-09-08', 'YYYY-MM-DD'), 28000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (15, TO_DATE('2024-12-01', 'YYYY-MM-DD'), 15000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (16, TO_DATE('2022-08-18', 'YYYY-MM-DD'), 30000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (17, TO_DATE('2023-04-12', 'YYYY-MM-DD'), 19000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (18, TO_DATE('2024-11-23', 'YYYY-MM-DD'), 21000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (19, TO_DATE('2022-10-05', 'YYYY-MM-DD'), 14000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (20, TO_DATE('2023-01-19', 'YYYY-MM-DD'), 25000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (21, TO_DATE('2024-07-07', 'YYYY-MM-DD'), 22000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (22, TO_DATE('2022-11-25', 'YYYY-MM-DD'), 17000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (23, TO_DATE('2023-05-15', 'YYYY-MM-DD'), 18000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (24, TO_DATE('2024-09-18', 'YYYY-MM-DD'), 26000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (25, TO_DATE('2022-02-13', 'YYYY-MM-DD'), 29000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (26, TO_DATE('2023-08-04', 'YYYY-MM-DD'), 20000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (27, TO_DATE('2024-03-16', 'YYYY-MM-DD'), 24000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (28, TO_DATE('2022-07-20', 'YYYY-MM-DD'), 21000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (29, TO_DATE('2023-10-02', 'YYYY-MM-DD'), 30000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (30, TO_DATE('2024-05-11', 'YYYY-MM-DD'), 19000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (31, TO_DATE('2022-09-27', 'YYYY-MM-DD'), 17000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (32, TO_DATE('2023-06-09', 'YYYY-MM-DD'), 15000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (33, TO_DATE('2024-10-22', 'YYYY-MM-DD'), 23000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (34, TO_DATE('2022-12-30', 'YYYY-MM-DD'), 16000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (35, TO_DATE('2023-03-11', 'YYYY-MM-DD'), 28000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (36, TO_DATE('2024-01-28', 'YYYY-MM-DD'), 24000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (37, TO_DATE('2022-05-22', 'YYYY-MM-DD'), 18000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (38, TO_DATE('2023-07-16', 'YYYY-MM-DD'), 29000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (39, TO_DATE('2024-08-03', 'YYYY-MM-DD'), 20000000);
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (40, TO_DATE('2022-10-29', 'YYYY-MM-DD'), 21000000);
+
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (60, TO_DATE('2022-03-21', 'YYYY-MM-DD'), 54730000);       
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (61, TO_DATE('2023-01-21', 'YYYY-MM-DD'), 32657000);       
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (62, TO_DATE('2024-08-18', 'YYYY-MM-DD'), 46095000);       
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (63, TO_DATE('2024-02-23', 'YYYY-MM-DD'), 19388000);       
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (64, TO_DATE('2023-09-01', 'YYYY-MM-DD'), 50540000);       
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (65, TO_DATE('2023-01-08', 'YYYY-MM-DD'), 16810000);       
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (66, TO_DATE('2024-08-12', 'YYYY-MM-DD'), 51460000);       
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (67, TO_DATE('2023-02-09', 'YYYY-MM-DD'), 32776000);       
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (68, TO_DATE('2023-01-03', 'YYYY-MM-DD'), 17314000);       
+INSERT INTO NguoiMua (id, ngayMua, giaSP) VALUES (69, TO_DATE('2022-10-27', 'YYYY-MM-DD'), 39156000);  
+
 
 SELECT * FROM CauHinhSP;
 SELECT * FROM danhsachsp;
